6d18801d02f229ac4c6ef31c4efa51b63301002b
Danielle <126515415+elleinadseer@users.noreply.github.com>
2024-09-02 12:27:11 +0000

    css and fix

--- javascript/PDFextraction.js
+++ javascript/PDFextraction.js
@@ -6,7 +6,6 @@ import { showMeasureRow } from './appearenceChanges.js';
 // Function to handle PDF extraction and form filling
 export function handlePdfExtraction() {
     document.getElementById('uploadPdf').addEventListener('change', function(e) {
-        clearForm();
 
         var file = e.target.files[0];
         var fileReader = new FileReader();
@@ -17,7 +16,6 @@ export function handlePdfExtraction() {
             pdfjsLib.getDocument(typedarray).promise.then(function(pdf) {
                 var allTextPromises = [];
 
-                // Loop through all the pages and get the text content
                 for (var i = 1; i <= pdf.numPages; i++) {
                     allTextPromises.push(pdf.getPage(i).then(function(page) {
                         return page.getTextContent().then(function(textContent) {
@@ -28,13 +26,11 @@ export function handlePdfExtraction() {
                     }));
                 }
 
-                // When all text contents are resolved, process the aggregated text
                 Promise.all(allTextPromises).then(function(pagesText) {
                     var text = pagesText.join(' ');
 
                     console.log("Extracted Text:", text);
 
-                    // Extract the data using regex
                     var extractedData = {
                         assessment_date: extractData(/Assessment Date\s+(\d{2}\/\d{2}\/\d{4})/, text),
                         submission_date: extractData(/Submission Date\s+(\d{2}\/\d{2}\/\d{4})/, text),
@@ -49,63 +45,44 @@ export function handlePdfExtraction() {
 
                     console.log("Extracted Data:", extractedData);
 
-                    // Extract the address using regex
                     var addressMatch = text.match(/Dwelling Address\s*(\d+),\s*(.+?),\s*([A-Z\s]+?),\s*([A-Z\d]{2,4}\s\d[A-Z]{2})/);
                     if (addressMatch) {
                         var addressNumber = addressMatch[1].trim();
                         var street = `${addressNumber} ${addressMatch[2].trim()}`;
                         var town = capitaliseFirstLetter(addressMatch[3].trim());
                         var postcode = addressMatch[4].trim();
-                    
-                        // Fill in the address fields
+
                         document.getElementById('postcode').value = postcode;
                         document.getElementById('street').value = street;
                         document.getElementById('town').value = town;
-                    
+
                         console.log('Address match results:', {
                             postcode: document.getElementById('postcode').value,
                             street: document.getElementById('street').value,
                             town: document.getElementById('town').value
                         });
 
-
                         searchSubmission(postcode).then(function(record) {
-                            // Log the data retrieved from searchSubmission function
                             console.log('Data retrieved from searchSubmission:', record);
-                        
-                            // Assuming you have form fields with these IDs and you want to fill them
                             selectDropdownOption('#schemeSelect', record.scheme);
                             document.getElementById('installDateInput').value = record.installDate || "";
-                        
-                            // Iterate over the record.measures array and fill the corresponding dropdowns
+
                             record.measures.forEach(function(measure, index) {
-                                // Determine the measure row index (1-based)
                                 const measureIndex = index + 1;
-
-                                // Show the respective measure row
                                 showMeasureRow(measureIndex);
-
-                                // Assuming measureList divs are named sequentially as m1measureList, m2measureList, etc.
                                 var dropdownId = '#m' + measureIndex + 'measureList';
                                 selectDropdownOption(dropdownId, measure);
-
-                                // Populate installer info for each measure
                                 populateInstallerInfo(measure, measureIndex);
                             });
 
                             document.getElementById('eligibility').value = record.eligibility || "";
-                        
-                            console.log('Record Install Date:', record.installDate); // Log to verify the date value
-                        
+                            console.log('Record Install Date:', record.installDate);
                         }).catch(function(error) {
                             console.error('Error retrieving data from Google Sheets:', error);
                         });
 
-                        // Search for postcode and address number, then fill form
                         searchPostcode(postcode, addressNumber).then(function(record) {
-                            // Log the data retrieved from the searchPostcode function
                             console.log('Data retrieved from searchPostcode:', record);
-
                             document.getElementById('sName').value = record.surname || "";
                             document.getElementById('fName').value = record.forename || "";
                             document.getElementById('URN').value = record.urn || "";
@@ -113,21 +90,18 @@ export function handlePdfExtraction() {
                             console.error('Error retrieving data from Google Sheets:', error);
                         });
 
-                        // Additional data extraction
                         var match = text.match(/Lowest floor\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)/);
                         if (match) {
                             var floorArea = match[1];
                             var roomHeight = match[2];
                             var wallPerimeter = match[3];
 
-                            // Log the extracted values
                             console.log('Extracted values:', {
                                 floorArea: floorArea,
                                 roomHeight: roomHeight,
                                 wallPerimeter: wallPerimeter
                             });
 
-                            // Fill the form fields with the extracted values
                             document.getElementById('floorArea').value = floorArea || "";
                             document.getElementById('roomHeight').value = roomHeight || "";
                             document.getElementById('wallPerimeter').value = wallPerimeter || "";
@@ -136,38 +110,25 @@ export function handlePdfExtraction() {
                         }
                     }
 
-                    // Fill the form fields with the extracted data
                     document.getElementById('surveyDate').value = extractedData.assessment_date || "";
                     document.getElementById('submissionDate').value = extractedData.submission_date || "";
                     document.getElementById('totalFloorArea').value = extractedData.total_floor_area || "";
                     document.getElementById('SAPrating').value = extractedData.current_efficiency_rating || "";
                     document.getElementById('rdsapNum').value = extractedData.reference_number || "";
 
-                    // Select the appropriate option in the dropdowns
                     selectDropdownOption('#roofType', extractedData.roof_type);
                     selectDropdownOption('#wallConstructSelect', extractedData.wall_type);
                     selectDropdownOption('#propertyType', extractedData.property_type);
                     selectDropdownOption('#YOpropSelect', extractedData.construction_year);
                     selectDropdownOption('#wallConstructSelect', extractedData.wall_type);
-                    selectDropdownOption('#wallConstructSelect', extractedData.wall_type);
-
-                    // Optionally submit the form
-                    // document.getElementById('myForm').submit();
                 });
-
             });
         };
-
-        function clearForm() {
-            var form = document.getElementById('yourFormId'); // Replace with your form's ID
-            if (form) {
-                form.reset(); // Reset form fields
-            }
-        }
 
         fileReader.readAsArrayBuffer(file);
     });
 }
+
 
 // Load Google API client and initialize PDF extraction
 gapi.load('client', initClient);
